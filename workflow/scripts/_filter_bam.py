4d826a757099a58dfcfbfa8420094f40e8477e59
kopardev <vishal.koparde@nih.gov>
2022-03-04 15:34:10 -0500

    bam filtering script

---
+++ workflow/scripts/_filter_bam.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+ Author: Vishal Koparde, PhD
+# Date: Jan 2022
+# This script take raw alignments, presorted and indexed and filtered them for reads
+# aligning as proper pairs with a set fragment size.
+# inputs:
+# @inputBAM: raw BAM file, sorted and indexed
+# @flagmentlength: default 1000, readpairs with fragment length larger than this integer are discarded
+# outputs:
+# @outputBAM: filtered output BAM file
+import pysam
+import argparse
+
+parser = argparse.ArgumentParser(description='Filter BAM by readids')
+parser.add_argument('--inputBAM', dest='inputBAM', type=str, required=True,
+                    help='input BAM file')
+parser.add_argument('--outputBAM', dest='outputBAM', type=str, required=True,
+                    help='filtered output BAM file')
+parser.add_argument('--fragmentlength', dest='fl', type=int, required=False, default=1000,
+                    help='discard flagment lengths larger than this integer')
+args = parser.parse_args()
+
+inBAM = pysam.AlignmentFile(args.inputBAM, "rb")
+outBAM = pysam.AlignmentFile(args.outputBAM, "wb", template=inBAM)
+
+count=0
+for read in inBAM.fetch():
+    count+=1
+    if count%1000000 == 0:
+        print("%d reads read!"%(count))
+    if not read.is_proper_pair or read.is_unmapped:
+        continue
+    if read.template_length > args.fragmentlength:
+        continue
+    outBAM.write(read)
+inBAM.close()
+outBAM.close()
