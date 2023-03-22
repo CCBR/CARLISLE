6f23df1e82b2eaa21a8adecfc81211658b501f3a
slsevilla <slsevilla@gmail.com>
2023-03-22 14:30:18 -0400

    remove tmp

--- workflow/scripts/_generate_spikein_wrapper.R
+++ workflow/scripts/_generate_spikein_wrapper.R
@@ -7,8 +7,6 @@ parser$add_argument("--rmd", type="character", required=TRUE,
                     help="path to rmd")
 parser$add_argument("--sourcefile", type="character", required=TRUE,
                     help="path to function file")
-parser$add_argument("--tmpdir", type="character", required=FALSE, default="/tmp",
-                    help = "tmpdir")
 parser$add_argument("--report", type="character", required=TRUE,
                     help = "HTML report")
 parser$add_argument("--bam_list", type="character", required=TRUE,
@@ -20,7 +18,6 @@ args <- parser$parse_args()
 debug="FALSE"
 if (debug){
   sourcefile="~/../../Volumes/Pipelines/CARLISLE_dev/workflow/scripts/_carlisle_functions.R"
-  tmpdir="/dev/shm"
   report="~/../../Volumes/data/tmp/carlisle/spike_report.html"
   bam_list="macs2/peak_output/53_H3K4me3_1_vs_nocontrol.dedup.broad.peaks.bed macs2/peak_output/53_H3K4me3_1_vs_nocontrol.dedup.narrow.peaks.bed macs2/peak_output/53_H3K4me3_2_vs_nocontrol.dedup.broad.peaks.bed macs2/peak_output/53_H3K4me3_2_vs_nocontrol.dedup.narrow.peaks.bed"
   spikein_control="NC_000913.3"
@@ -38,6 +35,4 @@ parameters=list(sourcefile=sourcefile,
 
 rmarkdown::render(args$rmd,
                   params=parameters,
-                  output_file = report,
-                  intermediates_dir = paste(tmpdir,"intermediates_dir",sep="/"),
-                  knit_root_dir = paste(tmpdir,"knit_root_dir",sep="/"))
+                  output_file = report)
